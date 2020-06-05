927960ddf750abc89ded1908c591d51dd73d2d89
wambosa <shondiaz@gmail.com>
2020-06-05 03:01:51 +0000

    fix build

--- infra/route53/certificate.tf
+++ infra/route53/certificate.tf
@@ -4,7 +4,7 @@ resource "aws_acm_certificate" "shon_diaz" {
   subject_alternative_names = ["${local.public_domain}"]
 }
 
-resource "aws_route53_record" "shon_diaz_cert_validation" 
+resource "aws_route53_record" "shon_diaz_cert_validation" {
   name    = aws_acm_certificate.shon_diaz.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.shon_diaz.domain_validation_options.0.resource_record_type
   zone_id = aws_route53_zone.shon_diaz.id
